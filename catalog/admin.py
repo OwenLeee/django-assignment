bc940925a182caba86c7c1f105538b3f5d890be9
Owen Lee <owen55889@gmail.com>
2026-09-21 15:42:04 -0700

    feat(admin): configure product list and prefetch tags

--- catalog/admin.py
+++ catalog/admin.py
@@ -17,4 +17,28 @@ class TagAdmin(admin.ModelAdmin):
     readonly_fields = ("created_at", "updated_at")
 
 
-admin.site.register(Product)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "description",
+        "category",
+        "display_tags",
+        "created_at",
+        "updated_at",
+    )
+    search_fields = (
+        "name",
+        "description",
+    )
+    list_filter = ("category", "tags")
+    readonly_fields = ("created_at", "updated_at")
+
+    @admin.display(description="Tags")
+    def display_tags(self, obj):
+        return ", ".join(tag.name for tag in obj.tags.all())
+
+    # Overriding get_queryset: Solving the N+1 query problem by prefetching related tags for products in the admin list view
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        return queryset.prefetch_related("tags")
