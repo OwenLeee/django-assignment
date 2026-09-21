13fcc0c1db7f6ec1f75e303b08bf4df4c5873748
Owen Lee <owen55889@gmail.com>
2026-09-21 01:38:24 -0700

    refactor(tests): split model tests by model

--- catalog/tests/models/test_category.py
+++ catalog/tests/models/test_category.py
@@ -2,7 +2,7 @@ from django.core.exceptions import ValidationError
 from django.db import IntegrityError, transaction
 from django.test import SimpleTestCase, TestCase
 
-from catalog.models import Category, Tag
+from catalog.models import Category
 
 
 class CategoryNameTests(SimpleTestCase):
@@ -76,76 +76,3 @@ class CategorySaveTests(TestCase):
         with self.assertRaises(ValidationError):
             category.save()
         self.assertEqual(Category.objects.count(), 0)
-
-
-class TagNameTests(SimpleTestCase):
-    def validate(self, tag):
-        tag.full_clean(
-            validate_unique=False,
-            validate_constraints=False,
-        )
-
-    def test_name_is_trimmed_and_case_is_preserved(self):
-        tag = Tag(name="  Industrial  ")
-
-        self.validate(tag)
-        self.assertEqual(tag.name, "Industrial")
-
-    def test_whitespace_only_name_is_rejected(self):
-        tag = Tag(name="   ")
-
-        with self.assertRaises(ValidationError) as context:
-            self.validate(tag)
-        self.assertIn("name", context.exception.message_dict)
-
-    def test_name_length_limit_applies_after_trimming(self):
-        tag = Tag(name=f"  {'A' * 100}  ")
-
-        self.validate(tag)
-        self.assertEqual(tag.name, "A" * 100)
-
-        tag.name = "A" * 101
-
-        with self.assertRaises(ValidationError) as context:
-            self.validate(tag)
-        self.assertIn("name", context.exception.message_dict)
-
-
-class TagUniquenessTests(TestCase):
-    def test_duplicate_name_is_rejected_ignoring_case_and_spaces(self):
-        Tag.objects.create(name="Industrial")
-        duplicate = Tag(name="  industrial  ")
-
-        with self.assertRaises(ValidationError):
-            duplicate.full_clean()
-
-    def test_existing_tag_can_change_its_own_capitalization(self):
-        tag = Tag.objects.create(name="Industrial")
-        tag.name = "INDUSTRIAL"
-        tag.full_clean()
-        tag.save()
-        tag.refresh_from_db()
-
-        self.assertEqual(tag.name, "INDUSTRIAL")
-
-    def test_database_rejects_case_insensitive_duplicate(self):
-        Tag.objects.create(name="Industrial")
-
-        with self.assertRaises(IntegrityError):
-            with transaction.atomic():
-                Tag.objects.create(name="industrial")
-
-
-class TagSaveTests(TestCase):
-    def test_create_stores_trimmed_name(self):
-        tag = Tag.objects.create(name="  Industrial  ")
-        tag.refresh_from_db()
-
-        self.assertEqual(tag.name, "Industrial")
-
-    def test_save_rejects_whitespace_only_name(self):
-        tag = Tag(name="   ")
-
-        with self.assertRaises(ValidationError):
-            tag.save()
-        self.assertEqual(Tag.objects.count(), 0)
