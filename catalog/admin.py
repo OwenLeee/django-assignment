3cb34cc064d31a198f5509e2d40e7f6d001320fc
Owen Lee <owen55889@gmail.com>
2026-09-21 15:25:07 -0700

    feat(admin): configure category list and edit form

--- catalog/admin.py
+++ catalog/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 
 from .models import Category, Product, Tag
 
-admin.site.register(Category)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "created_at", "updated_at")
+    search_fields = ("name",)
+    readonly_fields = ("created_at", "updated_at")
+
+
 admin.site.register(Product)
 admin.site.register(Tag)
