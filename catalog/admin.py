cd8a8851bd5e9240343d3a9e914c64897b03c390
Owen Lee <owen55889@gmail.com>
2026-09-21 14:52:56 -0700

    feat(admin): register catalog models

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,1 +1,7 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Category, Product, Tag
+
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Tag)
